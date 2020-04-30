0057445372757cc2b9abcde35db774eb6900da77
Anders Liss <Andersliss123@gmail.com>
2020-04-30 10:29:31 +0200

    Update arduino_secrets.h

--- include/arduino_secrets.h
+++ include/arduino_secrets.h
@@ -1,9 +1,9 @@
 // Fill in  your WiFi networks SSID and password
-#define SECRET_SSID "***REMOVED***"
-#define SECRET_PASS "***REMOVED***"
+#define SECRET_SSID "-" // Set WIFI ID (Using 2.4GHz, 5GHz does not work)
+#define SECRET_PASS "-" // Set WIFI passcode
 
 // Fill in the hostname of your AWS IoT broker
-#define SECRET_BROKER "***REMOVED***"
+#define SECRET_BROKER "---"
 
 // Fill in the boards public certificate
 const char SECRET_CERTIFICATE[] = R"(
